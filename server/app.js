fa324c8c211d54fafaea4efaffe5dda5d119aa97
MurshidtheDigitalNomad <murshidulhaqueahmed@gmail.com>
2025-06-07 03:13:13 +0600

    Fixing cors issue

--- server/app.js
+++ server/app.js
@@ -10,8 +10,20 @@ const wordsRoutes= require('./routes/words/words.router.js');
                                                      
 app.use(express.json());
 
+const cors = require('cors');
+
+const allowedOrigins = [ 'https://code-word-of-the-day.vercel.app',
+  /\.vercel\.app$/];
+
 app.use(cors({
-    origin: 'https://code-word-of-the-day.vercel.app'
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser clients like Postman
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 app.use('/api/users', userRoutes)
